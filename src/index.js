3c557b7a1b03cb4829e8c22c8c82cb2143af102a
washingtonaprodrigues-cmyk <washington.ap.rodrigues@gmail.com>
2026-05-30 04:13:31 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@ const app = express();
 
 app.use(express.json());
 
+const webhookRoutes = require('./routes/webhook');
 const formsRoutes = require('./routes/forms');
+
+app.use('/webhook', webhookRoutes);
 app.use('/forms', formsRoutes);
 
 app.get('/', (req, res) => {
